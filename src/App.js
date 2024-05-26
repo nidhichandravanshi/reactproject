d2b375546b20f3185cbc436a3f929dbc51c9f539
nidhichandravanshi <nidhichandravanshi39@gmail.com>
2024-05-26 09:39:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,127 @@
+// import React from 'react'
+// import {Card,Button } from 'react-bootstrap'
+// function App() {
+//   return (
+//   <>
+//   <h2>this is Component</h2>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="image/pic1.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>ss
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="img/pic2.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="image/pic3.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg/25%" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg/25%" />
+//       <Card.Body>ss
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src=".image/pic1.jpg/25%" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//    <Card style={{ width: '25%' }}>
+//       <Card.Img variant="top" src="./image/pic1.jpg/25%" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+  
+  
+  
+  
+//   </>
+  
+  
+//   )
+// }
+
+// export default App
